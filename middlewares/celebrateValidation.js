c7c11da4620f68c278a93b8ed27f35cc5232a13f
Alexey Ushakov <alexeyushakov16@gmail.com>
2024-01-25 16:44:48 +0300

    Pull request

--- middlewares/celebrateValidation.js
+++ middlewares/celebrateValidation.js
@@ -41,6 +41,6 @@ module.exports.creatingMovieValidation = celebrate({
 
 module.exports.moviesIdValidation = celebrate({
   params: Joi.object().keys({
-    movieId: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(),
+    movieId: Joi.string().required(),
   }),
 });
