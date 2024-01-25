c7eb81ebe6caf212342986a49de8841c5e46fa46
Alexey Ushakov <alexeyushakov16@gmail.com>
2024-01-25 16:15:00 +0300

    Pull request

--- utils/jwt.js
+++ utils/jwt.js
@@ -4,4 +4,4 @@ const { JWT_SECRET, NODE_ENV } = process.env;
 
 const generateToken = (payload) => jwt.sign(payload, NODE_ENV === 'production' ? JWT_SECRET : 'secret-key', { expiresIn: '7d' });
 
-module.exports = generateToken;
+module.exports = { generateToken };
